d60cc5b6b1d0d6f03b3f565f6ab9d90efe9528ff
Zaphyk <zaphyk@live.com>
2018-11-16 02:09:03 -0300

    progress on assets

--- Villages/assemble.sh
+++
@@ -1,1 +0,0 @@
-python "../../../hedra-tools/villages/execute.py" "../../../hedra-tools/villages/assemble.py" --folder="." --type=create
---
+++ Villages/house2/build.sh
@@ -0,0 +1,1 @@
+python "../../../hedra-tools/villages/build.py"
