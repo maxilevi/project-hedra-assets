28ba2025c43ae577a52128ff9d295ad9c5287801
Zaphyk <zaphyk@live.com>
2018-12-04 01:57:04 -0300

    lots of improvements for villages

---
+++ Villages/Blacksmith/build_colliders.sh
@@ -0,0 +1,1 @@
+python "../../../hedra-tools/villages/execute.py" "../../../hedra-tools/villages/assemble-colliders.py" --folder="./colliders" --type=create
--- Villages/house3/build.sh
+++
@@ -1,1 +0,0 @@
-python "../../../hedra-tools/villages/build.py"
